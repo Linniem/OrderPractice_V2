489de24e1c541c60093c1ea081fccd2715559c41
Linniem <Linniem@users.noreply.github.com>
2020-07-11 10:20:25 +0800

    Added files and JWT authentication

---
+++ OrderPractice_V2/Controllers/JwtController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderPractice_V2.Helpers;
+using OrderPractice_V2.ViewModels;
+
+namespace OrderPractice_V2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JwtController : ControllerBase
+    {
+        private readonly JwtHelper jwt;
+        public JwtController(JwtHelper jwt)
+        {
+            this.jwt = jwt;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("Test")]
+        public IActionResult Test()
+        {
+            return Ok(1);
+        }
+        [Authorize]
+        [HttpGet("Test2")]
+        public IActionResult Test2()
+        {
+            return Ok(2);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("signin")]
+        public ActionResult<string> SignIn([FromForm]LoginViewModel login)
+        {
+            if (ValidateUser(login))
+            {
+                return jwt.GenerateToken(login.Username);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        private bool ValidateUser(LoginViewModel login)
+        {
+            // TODO
+            return true;
+        }
+
+        [Authorize]
+        [HttpGet("claims")]
+        public IActionResult GetClaims()
+        {
+            return Ok(User.Claims.Select(p => new { p.Type, p.Value }));
+        }
+
+        [Authorize]
+        [HttpGet("username")]
+        public IActionResult GetUserName()
+        {
+            return Ok(User.Identity.Name);
+        }
+
+        [HttpGet("jwtid")]
+        public IActionResult GetUniqueId()
+        {
+            var jti = User.Claims.FirstOrDefault(p => p.Type == "jti");
+            return Ok(jti.Value);
+        }
+
+    }
+}
---
+++ OrderPractice_V2/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderPractice_V2.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
